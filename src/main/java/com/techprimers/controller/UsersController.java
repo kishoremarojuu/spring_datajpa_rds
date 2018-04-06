e93bf64f61178e852b2d004cf09991cef43ca917
panjalasunny <panjalasunny>
2018-04-06 01:57:22 -0500

    spring_datajpa_rds

---
+++ src/main/java/com/techprimers/controller/UsersController.java
@@ -0,0 +1,62 @@
+package com.techprimers.controller;
+
+import com.techprimers.model.Users;
+import com.techprimers.repository.UserJpaRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private UserJpaRespository userJpaRespository;
+    
+    @GetMapping(value = "/all")
+    public List<Users> findAll() {
+        return userJpaRespository.findAll();
+    }
+
+    @GetMapping(value = "/{name}")
+    public Users findByName(@PathVariable final String name){
+        return userJpaRespository.findByName(name);
+    }
+
+    @PostMapping(value = "/load")
+    public Users load(@RequestBody final Users users) {
+        userJpaRespository.save(users);
+        return userJpaRespository.findByName(users.getName());
+    }
+    
+  
+}
+
+//mySQL properties in application.properties 
+/*server.port=8080
+spring.datasource.url=jdbc:mysql://sql9.freesqldatabase.com:3306/sql9145487
+spring.datasource.username=sql9145487
+spring.datasource.password=dw4I6QLCZl
+spring.jpa.hibernate.ddl-auto=create-drop*/
+
+
+
+/*//JSON Request
+{
+	"id":3,
+    "name": "Asho",
+    "teamName": "tenet2",
+    "salary": 1000
+}*/
+
+
+/*server.port=8080
+spring.datasource.url=jdbc:mysql://awsmysql.csv2cf85mytg.us-west-2.rds.amazonaws.com:3306/awsmysql
+spring.datasource.username=awsmysql
+spring.datasource.password=Awsmysql12345
+spring.jpa.hibernate.ddl-auto=create-drop
+
+spring.datasource.driver-class-name=com.mysql.jdbc.Driver
+*/
+
