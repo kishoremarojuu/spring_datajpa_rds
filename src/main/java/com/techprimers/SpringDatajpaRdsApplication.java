5d4175606364bbafc96221e10a6dedd06f26cded
kishoremarojuu <kishoremarojuu@gmail.com>
2020-02-07 17:31:21 -0600

    added h2 code

--- src/main/java/com/techprimers/SpringDatajpaRdsApplication.java
+++ src/main/java/com/techprimers/SpringDatajpaRdsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDatajpaRdsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDatajpaRdsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDatajpaRdsApplication.class, args);
+    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+INSERT INTO 
+	TBL_EMPLOYEES (first_name, last_name, email) 
+VALUES
+  	('Lokesh', 'Gupta', 'howtodoinjava@gmail.com'),
+  	('ShivaKishore', 'Maroju', 'kishoremarojuu@gmail.com'),
+  	('John', 'Doe', 'xyz@email.com');
+
+
+
+/*
+INSERT INTO
+	TBL_PARKINGSPOT (licene_number)
+VALUES
+  	('jbxnh768');
+*/
