889ffc1c31cfcc7c9a2dc622a2720d5cd06a6482
kishoremarojuu <kishoremarojuu@gmail.com>
2020-08-12 20:34:16 -0500

    add test variables class

---
+++ src/main/java/com/techprimers/controller/TestVariables.java
@@ -0,0 +1,14 @@
+package com.techprimers.controller;
+
+public class TestVariables {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        float f=10.9f;
+        double d=52658;
+
+        short s=2;
+        byte bb = 4;
+
+    }
+}
